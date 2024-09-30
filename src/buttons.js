f586e9fe6822780c781b49dc9169973ab67bb3c8
Stefano Braconi <sugeppe@gmail.com>
2024-09-30 21:52:47 +0200

    second commit

--- src/buttons.js
+++ src/buttons.js
@@ -2,11 +2,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     const topRow = document.getElementById('top-row');
     const content = document.getElementById('content');
+    
   
     const buttons = [
       { id: 'Home', content: "Venite al nostro splendido ristorante, la cordialità del personale, il confort dell'ambiente e la squisitezza del menù vi stupiranno !!" },
       { id: 'Menu', content: 'PRIMI PIATTI: Penne alla amatriciana, penne alla boscaiola' },
-      { id: 'Contatti', content: 'Tel. 0333-0909090 Fax 0333-090901' }
+      { id: 'Contatti', content: 'Tel. 0333-0909090; Fax 0333-090901; email: sders@hjgjg.com' }
     ];
   
     buttons.forEach(button => {
